87edc993c553f53031255d0f8f6c96b5f34819f3
Bhavesh Shamnani <bcshamnani@gmail.com>
2021-02-04 00:14:59 +0530

    add cards

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,18 +2,21 @@ import React from "react";
 import Header from "./header";
 import Footer from "./footer";
 import Note from "./note";
-
+import notesObject from "../notes.js"
 
 function App() {
 	return (
 		<div>
 			<Header />
-			<Note />
+			{notesObject.map(obj =>
+				<Note
+					key={obj.key}
+					title={obj.title}
+					content={obj.content} />
+			)}
 			<Footer />
 		</div>
-
 	);
-}
-;
+};
 
 export default App;
