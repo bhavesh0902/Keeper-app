34f6b041ebc7b78c18a435e97f1b2593e4e3ea53
Bhavesh Shamnani <bcshamnani@gmail.com>
2021-02-04 21:26:39 +0530

    functionality added

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,24 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./header";
 import Footer from "./footer";
 import Note from "./note";
 import AddNote from "./addNote";
-import notesObject from "../notes.js"
 
 function App() {
-	return (
-		<div>
-			<Header />
-			<AddNote />
-			{notesObject.map(obj =>
-				<Note
-					key={obj.key}
-					title={obj.title}
-					content={obj.content} />
-			)}
-			<Footer />
-		</div>
-	);
-};
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div>
+      <Header />
+      <AddNote onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+}
 
 export default App;
