9075a28235c44ec72529f78e16119726ed3bcd89
Bhavesh Shamnani <bcshamnani@gmail.com>
2021-02-04 20:21:40 +0530

    addNOte page added

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "./header";
 import Footer from "./footer";
 import Note from "./note";
+import AddNote from "./addNote";
 import notesObject from "../notes.js"
 
 function App() {
 	return (
 		<div>
 			<Header />
+			<AddNote />
 			{notesObject.map(obj =>
 				<Note
 					key={obj.key}
